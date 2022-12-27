75c08c949dc83784df1239ab19d742c9aac3de61
Flavius <flavius.popa123@gmail.com>
2022-12-27 23:42:16 +0200

    initial commit, as i just made the project following along the tutorial, but I'm gonna try to add some things by myself soon

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# meditation
+# meditation 
+Made this meditation app while getting back at learning, after a quite long break.
+Besides of the html and css code, I am going to write down every single thing that I make in javascript to try to understand better what I am doing, while following along the tutorial of @developedbyed on youtube. 
+///
+Firstly, I am starting the javascript code by writing down a function called app, that is gonna be an arrow function. In the app function I am going to select the song by making the variable song, that is gonna select the song by using document.querySelector('.song')
---
+++ script.js
@@ -0,0 +1,86 @@
+const app = () => {
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container video");
+
+  //Sounds
+  const sound = document.querySelectorAll(".sound-picker button");
+  //Time Display
+  const timeDisplay = document.querySelector(".time-display");
+  const timeSelect = document.querySelectorAll('.time-select button')
+  //Get the length of the outline
+  const outlineLength = outline.getTotalLength();
+  console.log(outlineLength);
+  //Duration
+  let fakeDuration = 600;
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  //pick different sounds
+
+  sound.forEach(sound =>{
+    sound.addEventListener('click', function(){
+        song.src= this.getAttribute('data-sound');
+        video.src= this.getAttribute('data-video');
+        checkPlaying(song);
+    })
+  })
+
+  // play sound
+
+  play.addEventListener("click", () => {
+    checkPlaying(song);
+  });
+
+  //select sound
+
+  timeSelect.forEach(option =>{
+    option.addEventListener('click', function(){
+        fakeDuration = this.getAttribute('data-time');
+        timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`
+    })
+  })
+
+  //create a function specific to stop and play the sounds and also change the icons
+  const checkPlaying = (song) => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src= './svgs/pause.svg'
+    }else{
+        song.pause();
+        video.pause();
+        play.src = './svgs/play.svg'
+    }
+  };
+
+  //animate the circle and check the time 
+
+  song.ontimeupdate = () => {
+     let currentTime = song.currentTime;
+     let elapsed = fakeDuration - currentTime;
+     let seconds = Math.floor(elapsed % 60);
+     let minutes = Math.floor(elapsed / 60);
+
+     //animating the circle
+     let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+     outline.style.strokeDashoffset = progress;
+
+     //animate the text
+
+     timeDisplay.textContent= `${minutes}:${seconds}`;
+    
+    
+     //reset the song when the time is over
+     if(currentTime >= fakeDuration){
+        song.pause();
+        song.currentTime = 0;
+        play.src='./svgs/play.svg';
+        video.pause();
+     }
+  };
+};
+
+app();
